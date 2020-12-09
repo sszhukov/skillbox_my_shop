0cf2e8556b44019b4485a32088db8834e401a026
ss_zhukov <ss.zhukov11@gmail.com>
2020-12-09 22:44:50 +0300

    Added product component and product images

---
+++ src/ProductData/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Игровая консоль PlayStation 3',
+    price: '14 990 ₽',
+    imagePuth: 'img/products/ps3.png',
+  },
+  {
+    title: 'Игровая консоль PlayStation 4',
+    price: '19 990 ₽',
+    imagePuth: 'img/products/ps4.png',
+  },
+  {
+    title: 'Игровая консоль PlayStation 4 Slim',
+    price: '24 990 ₽',
+    imagePuth: 'img/products/ps4_slim.webp',
+  },
+  {
+    title: 'Игровая консоль PlayStation 4 Pro',
+    price: '32 990 ₽',
+    imagePuth: 'img/products/ps4_pro.webp',
+  },
+  {
+    title: 'Игровая консоль PlayStation 5',
+    price: '44 990 ₽',
+    imagePuth: 'img/products/ps5.png',
+  },
+  {
+    title: 'Игровая консоль Xbox360',
+    price: '12 990 ₽',
+    imagePuth: 'img/products/xbox360.png',
+  },
+  {
+    title: 'Игровая консоль Xbox One',
+    price: '17 990 ₽',
+    imagePuth: 'img/products/xbox_one.png',
+  },
+  {
+    title: 'Игровая консоль Xbox One S',
+    price: '22 990 ₽',
+    imagePuth: 'img/products/xbox_one_s.png',
+  },
+  {
+    title: 'Игровая консоль Xbox Series S',
+    price: '26 990 ₽',
+    imagePuth: 'img/products/xbox_series_s.png',
+  },
+  {
+    title: 'Игровая консоль Xbox Series X',
+    price: '44 990 ₽',
+    imagePuth: 'img/products/xbox_series_x.png',
+  },
+  {
+    title: 'Игровая консоль Nintendo Switch',
+    price: '23 990 ₽',
+    imagePuth: 'img/products/nintendo_switch.png',
+  },
+  {
+    title: 'Кружка Nescafe',
+    price: '1 350 ₽',
+    imagePuth: 'img/products/nescafe_mag.png',
+  },
+];
